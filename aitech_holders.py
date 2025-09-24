7b84227f3e8a53727b802557612fe49986d1a34c
Enai <moumid.soufiane@gmail.com>
2025-09-24 03:43:40 +0200

    Update aitech_holders.py

--- aitech_holders.py
+++ aitech_holders.py
@@ -13,8 +13,14 @@ urls = {
 }
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0 Safari/537.36'
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Referer": "https://google.com",
+    "Connection": "keep-alive",
+    "Upgrade-Insecure-Requests": "1"
 }
+
 
 
 def get_holders_data():
@@ -68,3 +74,4 @@ for chain, value in data.items():
         st.error(f"*{chain}*: {value}")
 
 st.markdown(f"### 🔢 Total holders across all chains: *{total}*")
+
